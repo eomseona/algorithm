5d1378be47d12540d416c2bcf256b10756ee48c4
eomseona <107669266+eomseona@users.noreply.github.com>
2023-09-28 13:21:23 +0900

    [Bronze II] Title: 소수, Time: 176 ms, Memory: 10044 KB -BaekjoonHub

---
+++ 백준/Bronze/2581. 소수/소수.js
@@ -0,0 +1,31 @@
+const fs = require("fs");
+const [M, N] = fs
+  .readFileSync("/dev/stdin")
+  .toString()
+  .split("\n")
+  .map((v) => +v);
+
+let primeArr = [];
+
+const isPrime = (n) => {
+  if (n === 1) return false;
+  for (let i = 2; i <= Math.sqrt(n); i++) {
+    if (n % i === 0) return false;
+  }
+  return true;
+};
+
+function solution() {
+  let sum = 0;
+  for (let i = M; i <= N; i++) {
+    if (i === 2 || (i % 2 !== 0 && isPrime(i))) {
+      primeArr.push(i);
+      sum += i;
+    }
+  }
+  if (primeArr.length) {
+    console.log(`${sum}\n${primeArr[0]}`);
+  } else console.log(-1);
+}
+
+solution();
