85c951a8f6b046376fe21bc7b4923dfc497bc52d
엄선아(Seona Eom) <eomsa98@gmail.com>
2024-04-30 19:27:41 +0900

    [level 1] Title: 개인정보 수집 유효기간, Time: 5.29 ms, Memory: 33.9 MB -BaekjoonHub

--- 프로그래머스/1/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.js
+++ 프로그래머스/1/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.js
@@ -1,5 +1,6 @@
 function solution(today, terms, privacies) {
-  today = today.split(".");
+  let [year, month, date] = today.split(".").map(Number);
+  nowdates = year * 12 * 28 + month * 28 + date;
   let termsObj = new Map();
   let expiredList = new Array();
 
@@ -10,38 +11,11 @@ function solution(today, terms, privacies) {
 
   privacies.forEach((p, idx) => {
     const [date, type] = p.split(" ");
-    const [startYear, startMonth, startDay] = date.split(".");
+    const [y, m, d] = date.split(".").map(Number);
     const term = termsObj.get(type);
-
-    let endDate = [
-      Number(startYear),
-      Number(startMonth) + term,
-      Number(startDay),
-    ];
-
-    if (endDate[1] > 12) {
-      if (endDate[1] % 12 === 0) {
-        endDate[0] = endDate[0] + Math.floor(endDate[1] / 12) - 1;
-        endDate[1] = 12;
-      } else {
-        endDate[0] = endDate[0] + Math.floor(endDate[1] / 12);
-        endDate[1] = endDate[1] % 12;
-      }
-    }
-
-    let expired = false;
-
-    for (let i = 0; i < endDate.length; i++) {
-      if (endDate[i] < Number(today[i])) {
-        expired = true;
-        break;
-      }
-      if (endDate[i] > Number(today[i])) break;
-      if (i === endDate.length - 1 && endDate[i] === Number(today[i]))
-        expired = true;
-    }
-
-    if (expired) expiredList.push(idx + 1);
+    const endDates = y * 12 * 28 + m * 28 + d + term * 28;
+    console.log(nowdates, endDates);
+    if (nowdates >= endDates) expiredList.push(idx + 1);
   });
   return expiredList;
 }
