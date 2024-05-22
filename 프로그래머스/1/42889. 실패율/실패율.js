a9a8deff8570a09d001753b5f666606e28bbc55c
엄선아(Seona Eom) <eomsa98@gmail.com>
2024-05-22 20:12:42 +0900

    [level 1] Title: 실패율, Time: 2.33 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/42889. 실패율/실패율.js
+++ 프로그래머스/1/42889. 실패율/실패율.js
@@ -1,17 +1,12 @@
 function solution(N, stages) {
-  let stay = {};
-  let percent = {};
+  let answer = [];
   let user = stages.length;
-  for (let i = 1; i <= N; i++) {
-    stay[i] = stages.filter((num) => num === i).reduce((acc) => (acc += 1), 0);
-  }
-  for (let i = 1; i <= N; i++) {
-    percent[i] = stay[i] / user;
-    user -= stay[i];
-  }
-  let answer = Array.from(Object.entries(percent))
-    .sort((a, b) => b[1] - a[1])
-    .map((v) => +v[0]);
 
-  return answer;
+  for (let i = 1; i <= N; i++) {
+    let staying = stages.filter((x) => x === i).length;
+    console.log(staying);
+    answer.push([i, staying / user]);
+    user -= staying;
+  }
+  return answer.sort((a, b) => b[1] - a[1]).map((v) => v[0]);
 }
