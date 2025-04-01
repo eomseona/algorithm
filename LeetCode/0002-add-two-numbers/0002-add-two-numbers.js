36a5ae3b12f65954c401bac046445b73f5d508fc
엄선아(Seona Eom) <eomsa98@gmail.com>
2025-04-01 20:38:31 +0900

    Time: 2 ms (90.5%), Space: 62.3 MB (9.72%) - LeetHub

--- LeetCode/0002-add-two-numbers/0002-add-two-numbers.js
+++ LeetCode/0002-add-two-numbers/0002-add-two-numbers.js
@@ -11,22 +11,27 @@
  * @return {ListNode}
  */
 var addTwoNumbers = function(l1, l2) {
-    let carry = 0;
-    let result = new ListNode(-1);
-    let dummy = result;
-    
-    while(l1 || l2 || carry ){
-        let l1Val = l1 ? l1.val : 0;
-        let l2Val = l2 ? l2.val : 0;
-        
-        let nextNode = (l1Val + l2Val + carry) % 10;
-        
-        result.next = new ListNode(nextNode);
-        result = result.next
-        
-        carry = Math.floor((l1Val + l2Val + carry)/10);
-        l1 = l1 ? l1.next : null;
-        l2 = l2 ? l2.next : null;
+    let dummy = new ListNode();
+    let res = dummy;
+    let total = 0, carry = 0;
+
+    while (l1 || l2 || carry) {
+        total = carry;
+
+        if (l1) {
+            total += l1.val;
+            l1 = l1.next;
+        }
+        if (l2) {
+            total += l2.val;
+            l2 = l2.next;
+        }
+
+        let num = total % 10;
+        carry = Math.floor(total / 10);
+        dummy.next = new ListNode(num);
+        dummy = dummy.next;
     }
-    return dummy.next;
+
+    return res.next;    
 };
