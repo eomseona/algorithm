4074730e3a88bbd5bf475fc481340215e7bf5c90
엄선아(Seona Eom) <eomsa98@gmail.com>
2025-07-10 12:26:30 -0400

    Time: 59 ms (16.49%), Space: 54.2 MB (76.77%) - LeetHub

--- LeetCode/0001-two-sum/0001-two-sum.js
+++ LeetCode/0001-two-sum/0001-two-sum.js
@@ -3,14 +3,15 @@
  * @param {number} target
  * @return {number[]}
  */
-var twoSum = function (nums, target) {
+var twoSum = function(nums, target) {
     let result = [];
-    for (let i = 0; i < nums.length; i++) {
-        for (let j = i + 1; j < nums.length; j++) {
-            if (nums[i] + nums[j] === target) {
-                return [i, j]; 
+    for(let i=0; i<nums.length; i++){
+        for(let j=1; j<nums.length; j++){
+            if(nums[i]+nums[j]===target && i!==j){
+                result = [i,j];
+                return result;
             }
         }
     }
-    return result; 
+    return result;
 };
