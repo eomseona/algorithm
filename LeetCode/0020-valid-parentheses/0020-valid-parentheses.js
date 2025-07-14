b55c8c98d140c85c9c49a3ea5c085d2075c56208
엄선아(Seona Eom) <eomsa98@gmail.com>
2025-07-14 17:01:13 -0400

    Time: 0 ms (100%), Space: 53.9 MB (95.97%) - LeetHub

--- LeetCode/0020-valid-parentheses/0020-valid-parentheses.js
+++ LeetCode/0020-valid-parentheses/0020-valid-parentheses.js
@@ -2,23 +2,34 @@
  * @param {string} s
  * @return {boolean}
  */
-var isValid = function(s) {
-        const stack = [];
-        const mapping = {
-            ')': '(',
-            '}': '{',
-            ']': '['
-        };
+var isValid = function (s) {
+  const lefts = new Map([
+    ["(", 0],
+    ["[", 1],
+    ["{", 2],
+  ]);
+  const rights = new Map([
+    [")", 0],
+    ["]", 1],
+    ["}", 2],
+  ]);
+  let stack = [];
 
-        for (const c of s) {
-            if (Object.values(mapping).includes(c)) {
-                stack.push(c);
-            } else if (mapping.hasOwnProperty(c)) {
-                if (!stack.length || mapping[c] !== stack.pop()) {
-                    return false;
-                }
-            }
-        }
+  for (let i = 0; i < s.length; i++) {
+    const char = s[i];
 
-        return stack.length === 0;    
+    if (lefts.has(char)) {
+      stack.push(char);
+    } else {
+      if (stack.length === 0) return false;
+
+      const lastLeft = stack.pop();
+      if (lefts.get(lastLeft) !== rights.get(char)) {
+        return false;
+      }
+    }
+  }
+    return stack.length === 0;
 };
+
+console.log(isValid("(]"));
