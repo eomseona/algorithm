58a77688441ad5d41f5251cf9bdf80338042699e
엄선아(Seona Eom) <eomsa98@gmail.com>
2025-09-02 15:03:02 -0400

    Time: 0 ms (100%), Space: 55.9 MB (98.85%) - LeetHub

--- LeetCode/0792-binary-search/0792-binary-search.js
+++ LeetCode/0792-binary-search/0792-binary-search.js
@@ -4,17 +4,14 @@
  * @return {number}
  */
 var search = function(nums, target) {
-  let left = 0;
-  let right = nums.length - 1;
-  while (left <= right) {
-    let mid = Math.floor((left + right) / 2);
-    if (nums[mid] === target) {
-      return mid;
-    } else if (nums[mid] < target) { 
-      left = mid + 1; 
-    } else {
-        right = mid - 1;
+    let left = 0;
+    let right = nums.length-1;
+    let mid;
+    while(nums[mid]!==target && left<=right){
+        mid = Math.floor((right+left)/2);
+        if(nums[mid] > target) right = mid - 1;
+        else if(nums[mid] < target) left = mid + 1;
+        else return mid;
     }
-  }
-  return -1;
+    return -1;
 };
