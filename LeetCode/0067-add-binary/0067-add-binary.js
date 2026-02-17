3d4a5817ce85b8661386b2840386e886ddcadbd9
엄선아(Seona Eom) <eomsa98@gmail.com>
2026-02-17 11:23:58 -0500

    Time: 3 ms (20.26%), Space: 55.8 MB (34.48%) - LeetHub

--- LeetCode/0067-add-binary/0067-add-binary.js
+++ LeetCode/0067-add-binary/0067-add-binary.js
@@ -4,6 +4,20 @@
  * @return {string}
  */
 var addBinary = function(a, b) {
-    let sum = BigInt("0b" + a) + BigInt("0b" + b);
-    return sum.toString(2);
+    a = a.split("");
+    b = b.split("");
+    const result = [];
+    let carry = 0;
+
+    while(a.length || b.length || carry){
+        const numA = a.length ? Number(a.pop()) : 0;
+        const numB = b.length ? Number(b.pop()) : 0;
+
+        const sum = numA + numB + carry;
+
+        result.unshift(sum % 2);
+        carry = Math.floor(sum / 2);
+    }
+    
+    return result.join("");
 };
