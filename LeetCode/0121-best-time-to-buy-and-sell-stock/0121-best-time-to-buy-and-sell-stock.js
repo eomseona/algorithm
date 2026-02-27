ecdd08cc4d6c37939e2667f6c487050af37ae857
엄선아(Seona Eom) <eomsa98@gmail.com>
2026-02-27 17:36:28 -0500

    Time: 3 ms (60.58%), Space: 65.4 MB (45.67%) - LeetHub

--- LeetCode/0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.js
+++ LeetCode/0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.js
@@ -2,15 +2,16 @@
  * @param {number[]} prices
  * @return {number}
  */
-var maxProfit = function(prices) {
-        let buy = prices[0];
-        let profit = 0;
-        for (let i = 1; i < prices.length; i++) {
-            if (prices[i] < buy) {
-                buy = prices[i];
-            } else if (prices[i] - buy > profit) {
-                profit = prices[i] - buy;
-            }
+var maxProfit = function (prices) {
+    let min = prices[0];
+    let profit = 0;
+
+    for (let i = 1; i < prices.length; i++) {
+        if (prices[i] < min) {
+            min = prices[i]
         }
-        return profit;
+
+        profit = Math.max(profit, prices[i] - min);
+    }
+    return profit;
 };
